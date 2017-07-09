8abde561d7d24c7ad137b7b692397e3a15634443
Haytham <Haytham@Foudroid-PC.aag-de.ibmmobiledemo.com>
2017-07-09 09:13:01 +0200

    Second

--- RESTful-app/WebContent/controller.js
+++ RESTful-app/WebContent/controller.js
@@ -1,7 +1,13 @@
 angular.module('myApp', [])
-.controller('weatherCtrl', function($scope, $http) {
-    $http.get('http://api.openweathermap.org/data/2.5/weather?q=cairo&APPID=741df2b3e5b906883724dccead21b911').
-        then(function(response) {
-            $scope.weather = response.data;
-        });
+.controller('weatherCtrl', function($scope, $http)
+{
+	$scope.getWeather = function(city)
+	{
+		 $http.get('http://api.openweathermap.org/data/2.5/weather?q='+city+'&APPID=741df2b3e5b906883724dccead21b911').then(resp)		
+	};
+    resp = function(response)
+    {
+    	$scope.weather = response.data;
+    	
+    };
 });
